df87b7b0f926ffb91fea83b6ef39186ebdcc3617
Poshtaklon <152329594+YuliaDruzhynina@users.noreply.github.com>
2024-03-20 21:46:13 +0200

    Add files via upload

--- flask-docker-app/app.py
+++ flask-docker-app/app.py
@@ -1,1 +1,12 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello Worooooooold!"
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
